c4cbc2ce7b8006e8c05e5f40921a37b3f96dea00
voidiii <z798593356@gmail.com>
2024-08-05 23:55:46 +0800

    basic function done, still need to modify to improve the effect

--- HW1/My project/Assets/Rigid_Bunny.cs
+++ HW1/My project/Assets/Rigid_Bunny.cs
@@ -61,20 +61,91 @@ public class Rigid_Bunny : MonoBehaviour
 		return A;
 	}
 
+	bool Collision_Detection(Vector3 x_i, Vector3 v_i, Vector3 P, Vector3 N)
+	{
+		// In this function, detect the collision with the plane <P, N>
+		// If collision happens, update v and w
+		if (Vector3.Dot(x_i - P, N) < 0 && Vector3.Dot(v_i, N) < 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public static Matrix4x4 Subtract(Matrix4x4 a, Matrix4x4 b)
+    {
+        Matrix4x4 result = new Matrix4x4();
+
+        // Iterate through each element and perform subtraction
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                result[i, j] = a[i, j] - b[i, j];
+            }
+        }
+
+        return result;
+    }
+
 	// In this function, update v and w by the impulse due to the collision with
 	//a plane <P, N>
 	void Collision_Impulse(Vector3 P, Vector3 N)
 	{
 		MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+		Vector3[] vertices = mesh.vertices;
 
-        if (meshFilter != null)
+		Vector3 x = transform.position;
+		Matrix4x4 R = Matrix4x4.Rotate(transform.rotation);
+		Vector3 X = Vector3.zero;
+		int num = 0;
+
+        foreach (Vector3 vertex in vertices)
         {
-            // Get the Mesh from the MeshFilter
-            Mesh mesh = meshFilter.mesh;
-
-			int vertexCount = mesh.vertexCount;
-            Debug.Log("Vertex number: " + vertexCount);
+            Vector3 x_i = R.MultiplyPoint3x4(vertex) + x;
+			Vector3 v_i = v + Vector3.Cross(w, R.MultiplyPoint3x4(vertex));
+			if (Collision_Detection(x_i, v_i, P, N))
+			{
+				X += x_i;
+				num++;
+			}
         }
+
+		if (num == 0) return;
+
+		// at this point, X is actually sum of x_i + Rr_i
+		X = X / num;
+		X = X - x;
+		// after subtracting x, X is now Rr_i, so need to multiply R
+		Vector3 V = v + Vector3.Cross(w, X);
+		Vector3 Vn = Vector3.Dot(V, N) * N;
+		Vector3 Vt = V - Vn;
+		Matrix4x4 I = R * I_ref * R.inverse;
+
+		float a = Mathf.Max(1 - 0.5f * (1 + restitution) * Vn.magnitude / Vt.magnitude, 0);
+		Vector3 V_new = - restitution * Vn + a * Vt;
+
+		Matrix4x4 K = Matrix4x4.identity;
+		for(int i = 0; i < 4; i++)
+		{
+			K[i, i] /= mass;
+		}
+		
+		K = Subtract(K, Get_Cross_Matrix(X) * I.inverse * Get_Cross_Matrix(X));
+		Vector3 J = K.inverse * (V_new - V);
+
+		v += J / mass;
+		w += I.inverse.MultiplyVector(Vector3.Cross(X, J));
+		
+		// 衰减碰撞因子
+		if (v.magnitude < 0.01f)
+		{
+			restitution = 0;
+		}
+		Debug.Log("J / mass: " + J / mass);
+		Debug.Log("I.inverse.MultiplyVector(Vector3.Cross(R.MultiplyPoint3x4(X), J)): " + I.inverse.MultiplyVector(Vector3.Cross(X, J)));
+        
 	}
 
 	// Update is called once per frame
@@ -89,7 +160,7 @@ public class Rigid_Bunny : MonoBehaviour
 		}
 		if(Input.GetKey("l"))
 		{
-			v = new Vector3 (0.5f, 0.2f, 0);
+			v = new Vector3 (5f, 2f, 0);
 			w = new Vector3 (2f, 0, 0);
 			launched=true;
 		}
@@ -97,7 +168,8 @@ public class Rigid_Bunny : MonoBehaviour
 		// Part I: Update velocities
 		if(launched)
 		{
-			v = v + new Vector3(0, -0.01f, 0);
+			v = v + new Vector3(0, -9.8f, 0) * dt;
+			w = w * angular_decay;
 
 			// Part II: Collision Impulse
 			Collision_Impulse(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0));
@@ -112,7 +184,6 @@ public class Rigid_Bunny : MonoBehaviour
 			//Update angular status
 			Quaternion q = transform.rotation;
 			Quaternion deltaRotation = Quaternion.Euler(w * Mathf.Rad2Deg * dt);
-			w = w * angular_decay;
 			q = q * deltaRotation;
 
 			// Part IV: Assign to the object
